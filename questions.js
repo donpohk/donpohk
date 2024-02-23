4359e4d260c885d00646c85cbafd1c6871784c11
donpohk <161088994+donpohk@users.noreply.github.com>
2024-02-23 17:51:44 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,164 @@
+questions = [
+    {
+        "question": "Immigration from 3rd world countries should be banned.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -3,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Some cultures are better than others",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Abortion should be illegal in most cases.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I support policies that aim to address income inequality.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Should the government play a role in providing access to affordable healthcare for all citizens?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Do you believe in the importance of environmental conservation and addressing climate change?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Should there be efforts to reform the criminal justice system to promote fairness and reduce systemic issues?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Do you support measures to promote diversity, equity, and inclusion in various aspects of society?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Should there be stricter regulations on certain industries to protect consumers and the environment?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Should nations have open borders?",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Do you believe in the importance of social safety nets to assist those in need?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The means of production should belong to the workers who use them.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Military action by our nation is often necessary to protect it.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Should there be efforts to ensure equal opportunities for all individuals, regardless of their background?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Do you believe in the necessity of international collaboration in scientific research and innovation?",
+        "effect": {
+            "econ": 5,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Do you support the idea of a global currency to facilitate international trade?",
+        "effect": {
+            "econ": 10,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Taxes should be increased on the rich to provide for the poor.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Should there be efforts to promote cultural exchange and understanding between different nations?",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
